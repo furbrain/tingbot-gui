3a777765849f7d4d81e4e097c2ef2d0cfd9af12c
Phil Underwood <beardydoc@gmail.com>
2016-01-13 00:08:40 +0000

    Basic setting up for being a new module

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='tingbot-gui',
+      version='0.1',
+      description='Graphical User Interface for tingbot',
+      url='http://github.com/furbrain/tingbot-gui',
+      author='Phil Underwood',
+      author_email='beardydoc@gmail.com',
+      license='BSD',
+      packages=['tingbot_gui'],
+      package_dir={'tingbot_gui':'tingbot_gui'},
+      install_requires=['tingbot'],
+      dependency_links=['https://github.com/tingbot/tingbot-python/tarball/master'],
+      zip_safe=False,
+      keywords='tingbot',)
+
