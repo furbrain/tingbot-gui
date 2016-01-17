51bae81aff88897055e05dc30e2fa01096095bcb
Phil Underwood <beardydoc@gmail.com>
2016-01-17 23:41:54 +0000

    Add PopupButtons to gui module

--- tingbot_gui/__init__.py
+++ tingbot_gui/__init__.py
@@ -10,7 +10,7 @@ from .scrollarea import ScrollArea
 from .notebook import NoteBook
 
 from .dialog import MessageBox
-from .popupmenu import PopupMenu
+from .popupmenu import PopupMenu, PopupButton
 from .style import Style, get_default_style
 
 __author__ = 'Phil Underwood'
