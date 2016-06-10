d5199c4d6f7fddf7b85286fb84dc7c2c229a7628
Phil Underwood <beardydoc@gmail.com>
2016-06-10 23:52:40 +0100

    Further work on keyboard
    Add pngs for special keys
    Make text area work

--- tingbot_gui/keyboard.py
+++ tingbot_gui/keyboard.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-import pygame
+import os
 from functools import partial
+
+import pygame
 
 from .dialog import ModalWindow, MessageBox
 from .button import Button
@@ -8,6 +10,20 @@ from .container import Panel
 from .statictext import StaticText
 from .widget import Widget
 from tingbot.graphics import _xy_from_align
+
+def get_image_location(name,inverted=False):
+    directory = os.path.dirname(__file__)
+    text = "image:"
+    if inverted:
+        text += os.path.join(directory,'images',name)+"_inv.png"
+        text += "|"
+        text += os.path.join(directory,'images',name)+".png"
+    else:
+        text += os.path.join(directory,'images',name)+".png"
+        text += "|"
+        text += os.path.join(directory,'images',name)+"_inv.png"
+    return text
+    
 
 class KbButton(Button):
     def __init__(self, xy, size, parent, keys, style,):
@@ -87,35 +103,39 @@ class Keyboard(ModalWindow):
         
         self.text = KbText((7,46), (307,36), 'topleft', self, self.style, text)
         self.layout = 0
-
+        #find image files
+        shift_images = get_image_location("shift")
+        inverted_shift_images = get_image_location("shift", inverted=True)
+        del_images = get_image_location("del")
+        enter_images = get_image_location("enter")
+        smiley_images = get_image_location("smiley")
+        style14 = self.style.copy(button_text_font_size=14)
+        style13 = self.style.copy(button_text_font_size=13)
+        style12 = self.style.copy(button_text_font_size=12)
+        
         #create all our buttons
-        cancel_button = CancelButton((16,23), (60,24), "left", self, self.style, "Cancel", "left")
-        ok_button = OkButton((304,23), (60,24), "right", self, self.style, "OK", "right")
-        self.title = StaticText((160,23),(160,24),"center",self,self.style,title,"center")
+        cancel_button = CancelButton((16,23), (60,24), "left", self, style12, "Cancel", "left")
+        ok_button = OkButton((304,23), (60,24), "right", self, style12, "OK", "right")
+        self.title = StaticText((160,23),(160,24),"center",self,style12,title,"center")
         for row,x,y in zip(zip(*self.keys),(7,22,53),(0,33,66)):
             for i,letter in enumerate(zip(*row)):
-                KbButton((x+i*31,105+y),(28,28),self,letter,self.style)
-        space_bar = Button((73,105+99),(175,28),"topleft",self,self.style,"space",callback=partial(self.text.add_letter,' '))
-        shift = ShiftButton((7,105+66),(34,28),self,"ABCD",self.style)
-        alnum = AlnumButton((7,105+99),(61,28),self,"1234",self.style)
-        delete = Button((314,105+66),(34,28),"topright",self,self.style,'<',callback=self.text.del_letter)
-        enter = Button((314,105+99),(28,28),"topright",self,self.style,'/',callback=self.new_line)
-        emoji = Button((253,105+99),(28,28),"topleft",self,self.style,'@',callback=self.emoji)
+                KbButton((x+i*31,105+y),(28,28),self,letter,style14)
+        space_bar = Button((73,105+99),(175,28),"topleft",self,style13,"space",callback=partial(self.text.add_letter,' '))
+        shift_labels = [shift_images, inverted_shift_images, u'#+=', u'123']
+        shift = ShiftButton((7,105+66),(34,28),self,shift_labels,style13)
+        alnum = AlnumButton((7,105+99),(61,28),self,['@123','@123','abc','abc'],style13)
+        delete = Button((314,105+66),(34,28),"topright",self,self.style,del_images,callback=self.text.del_letter)
+        enter = Button((314,105+99),(28,28),"topright",self,self.style,enter_images,callback=self.new_line)
+        emoji = Button((253,105+99),(28,28),"topleft",self,self.style,smiley_images,callback=self.emoji)
         self.set_layout(0)
                 
     def set_layout(self,layout):    
         self.layout = layout
         self.update(downwards=True)
                 
-    def add_letter(self,letter):
-        print letter
-        
-    def del_letter(self):
-        print "del"
-        
     def new_line(self):
-        print "NL"
+        self.close(self.text.string)
         
     def emoji(self):
-        MessageBox(message="Emojiiiiis!")
+        MessageBox(message="Sorry, no emoji's yet")
         
